6722c4caddb5aa9c1edb6652b414b6218c85b5dd
John-Thailand <naoki.honda@mirrorfit.jp>
2025-06-09 18:02:56 +0900

    Setting up a Database Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from "@nestjs/common";
 import { UsersModule } from "./users/users.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./users/user.entity";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    // TypeORMを使ってSQLiteデータベースに接続するための初期設定
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
