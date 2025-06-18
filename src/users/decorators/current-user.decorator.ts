3d7eb2a9322ff20489b166dca838e6e84cf4d552
John-Thailand <naoki.honda@mirrorfit.jp>
2025-06-18 18:23:16 +0900

    デコレータ内でリクエスト内のセッションのユーザーIDを取得

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -4,7 +4,12 @@ import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 // コントローラーのハンドラメソッドの引数に対してカスタムデータを注入するための仕組み
 // 例：getProfile(@CurrentUser() user: string) {
 export const CurrentUser = createParamDecorator(
-  (data: any, context: ExecutionContext) => {
-    return 'hi there'
+  (data: never, context: ExecutionContext) => {
+    // パラメータデコレータはDIシステムの外部に存在します。
+    // そのため、デコレータはユーザーサービスのインスタンスを取得できません
+    // なのでインターセプターでユーザーサービスからユーザー情報を取得し、デコレータに返すようにする
+    const request = context.switchToHttp().getRequest()
+    console.log(request.session.userId)
+    return 'hi there!'
   },
 )
