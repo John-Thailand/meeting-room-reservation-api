1c3b1972157b8fa5a528305d55cb0aafaecfd8cf
John-Thailand <naoki.honda@mirrorfit.jp>
2025-06-20 00:06:42 +0900

    CreateUserDtoでパスワードに8文字以上16文字以下などのバリデーションを設定

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,11 +1,15 @@
 import { Type } from 'class-transformer';
-import { IsDate, IsEmail, IsString } from 'class-validator';
+import { IsDate, IsEmail, IsString, Length, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   email: string;
 
   @IsString()
+  @Length(8, 16)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,16}$/, {
+    message: 'your password must contain at least one lowercase letter, one uppercase letter, one number, and one symbol',
+  })
   password: string;
 
   // 文字列 "2025-06-12" をDate型に変換
