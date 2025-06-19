cfc686c468cb12ab552210f1fa185d4bd8dab72d
John-Thailand <naoki.honda@mirrorfit.jp>
2025-06-19 18:00:47 +0900

    configで.envを適用する

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,31 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./users/user.entity";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
     // TypeORMを使ってSQLiteデータベースに接続するための初期設定
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User],
-      synchronize: true,
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User],
+          synchronize: true,
+        }
+      }
     }),
     UsersModule,
   ],
