f0414a461939203a5ad50571c1aef939eb555d24
John-Thailand <naoki.honda@mirrorfit.jp>
2025-06-20 19:47:59 +0900

    コメント追記

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Cookie-Sessionミドルウェアを導入し、セッション管理を可能にする
   // セッション情報をクッキーに直接保存する
+  // app.useでExpressのミドルウェアを登録し、NestJSで実装したミドルウェアはconfigure()で登録する
   app.use(cookieSession({
     // セッションデータの暗号化に使用される秘密鍵
     keys: ['asdfasdf']
