b187e5481e27f7da844310fca2436bd2262b67d4
John-Thailand <naoki.honda@mirrorfit.jp>
2025-06-26 14:01:20 +0900

    会議室作成APIの実装

--- src/meeting-rooms/meeting-rooms.controller.ts
+++ src/meeting-rooms/meeting-rooms.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
+import { MeetingRoomsService } from './meeting-rooms.service';
+import { MeetingRoom } from './meeting-room.entity';
+import { CreateMeetingRoomDto } from './dtos/create-meeting-room.dto';
 
-@Controller('meeting-rooms')
-export class MeetingRoomsController {}
+@Controller()
+export class MeetingRoomsController {
+  constructor(private meetingRoomsService: MeetingRoomsService) {}
+
+  @Post('coworking-spaces/:coworking_space_id/meeting-rooms')
+  async createMeetingRoom(
+    @Param('coworking_space_id') coworkingSpaceId: string,
+    @Body() body: CreateMeetingRoomDto,
+  ): Promise<MeetingRoom> {
+    const meetingRoom = await this.meetingRoomsService.create(coworkingSpaceId, body.name)
+    return meetingRoom
+  }
+}
--- src/meeting-rooms/meeting-rooms.service.ts
+++ src/meeting-rooms/meeting-rooms.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MeetingRoom } from './meeting-room.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MeetingRoomsService {}
+export class MeetingRoomsService {
+  constructor(@InjectRepository(MeetingRoom) private repo: Repository<MeetingRoom>) {}
+
+  async create(
+    coworkingSpaceId: string,
+    name: string,
+  ): Promise<MeetingRoom> {
+    // コワーキングスペースが存在しない場合、エラーを返す
+    // TODO: コワーキングのrepoを使わないと
+    const coworkingSpace = await this.repo.findOneBy({
+      id: coworkingSpaceId,
+      is_deleted: false
+    })
+    if (!coworkingSpace) {
+      throw new NotFoundException('coworking space not found')
+    }
+
+    // そのコワーキングスペースに同じ名前の会議室が既に存在している場合、エラーを返す
+    const existingMeetingRoom = await this.repo.findOneBy({
+      coworking_space_id: coworkingSpace.id,
+      name
+    })
+    if (existingMeetingRoom) {
+      throw new BadRequestException('already exist the meeting room')
+    }
+
+    const meetingRoom = this.repo.create({
+      coworking_space_id: coworkingSpaceId,
+      name
+    })
+
+    return this.repo.save(meetingRoom)
+  }
+}
