b3baefa04372d272ce9cd38c0d5a9aa265b4001a
John-Thailand <naoki.honda@mirrorfit.jp>
2025-06-27 22:18:56 +0900

    Reservationのエンティティを定義

---
+++ src/reservations/reservation.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Reservation {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text', nullable: false })
+  meeting_room_id: string;
+
+  @Column({ type: 'text', nullable: false })
+  user_id: string;
+
+  @Column({ type: 'datetime', nullable: false })
+  start_datetime: Date;
+
+  @Column({ type: 'datetime', nullable: false })
+  end_datetime: Date;
+
+  @Column({ type: 'boolean', default: false, nullable: false })
+  is_deleted: boolean;
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP', nullable: false })
+  created_at: Date;
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP', nullable: false })
+  updated_at: Date;
+}
