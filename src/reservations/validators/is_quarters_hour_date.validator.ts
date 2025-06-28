4e84f7d7bb74714a86ff65904c71ffaeef61c948
John-Thailand <naoki.honda@mirrorfit.jp>
2025-06-28 13:07:09 +0900

    registerDecoratorのそれぞれのプロパティについて理解するためにコメント追記

--- src/reservations/validators/is_quarters_hour_date.validator.ts
+++ src/reservations/validators/is_quarters_hour_date.validator.ts
@@ -1,13 +1,17 @@
 import { registerDecorator, ValidationArguments, ValidationOptions } from "class-validator";
 
-export function IsQuartersHourDate(validationOptions?: ValidationOptions) {
+// カスタムバリデーションデコレータ
+// リクエストボディのプロパティの値をチェックする
+// validationOptions: このデコレータが受け取るオプション
+export function IsQuartersHourDate() {
   return (object: any, propertyName: string) =>
     registerDecorator({
-      name: 'IsQuartersHourDate',
+      name: 'IsQuartersHourDate', // 識別名
+      // どのクラスにこのバリデーションを紐づけるか
+      // 例（object.constructor = class CreateMyReservationDto）
       target: object.constructor,
-      propertyName,
-      constraints: [],
-      options: validationOptions,
+      propertyName, // 例：start_datetime end_datetime
+      constraints: [], // 外部から渡された制約値を validator.validate() や defaultMessage() で使うための配列
       validator: {
         validate(value: any, args: ValidationArguments) {
           // そもそもDate型の値かどうか確認しておく
@@ -17,20 +21,17 @@ export function IsQuartersHourDate(validationOptions?: ValidationOptions) {
 
           // ミリ秒や秒は0であるか確認しておく
           const milliseconds = value.getMilliseconds()
-          console.log(milliseconds)
           if (milliseconds !== 0) {
             return false
           }
 
           const seconds = value.getSeconds()
-          console.log(seconds)
           if (seconds !== 0) {
             return false
           }
 
           // 15分間隔の場合は問題なし
           const minutes = value.getMinutes()
-          console.log(minutes)
           return minutes % 15 === 0
         },
         defaultMessage(args: ValidationArguments) {
